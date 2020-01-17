bc2b1fe7a0db6d8adc3851a5c3eb4bf8edb863a8
Malin Schoening <malin.schoening@gmail.com>
2020-01-17 13:29:12 +0100

    Initial Commit

---
+++ nailartmalinapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nailartmalinapp
+//
+//  Created by Malin Schoening on 2020-01-17.
+//  Copyright © 2020 Malin Schoening. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
